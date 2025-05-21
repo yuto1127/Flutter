3816b1b707a31f274d4624a9b3fc243cd3ab8069
yuto1127 <yuto.aka1127@gmail.com>
2025-05-21 15:39:00 +0900

    変更追加

--- Flutter/src/20250520/sample/lib/main.dart
+++ Flutter/src/20250520/sample/lib/main.dart
@@ -1,23 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  final title = 'Flutterサンプル';
+  final message = 'サンプル・メッセージ。';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello Flutter!'),
-        ),
-        body: Text(
-          'Hello,Flutter World!!',
-          style: TextStyle(fontSize: 32.0),
-        ),
-      ),
+      home: MyHomePage(title: this.title, message: this.message),
     );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  final String message;
+  const MyHomePage({
+    Key? key,
+    required this.title,
+    required this.message
+  }) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Text(
+        widget.message,
+        style: TextStyle(fontSize: 32.0),
+      ),
+    ); // カンマをセミコロンに修正
   }
 }
