b7c52b52bb6c2cafc82b828b075d9b8824615901
yuto1127 <yuto.aka1127@gmail.com>
2025-06-10 16:29:23 +0900

    ちど

--- Flutter/src/todo/lib/main.dart
+++ Flutter/src/todo/lib/main.dart
@@ -1,122 +1,155 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyApp()); // const を追加
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key}); // const と super.key を追加
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return MaterialApp( // new キーワードはDart 2.0以降不要になったため削除
+      title: 'Todo App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.blue,
+        primaryColor: const Color(0xFF2196f3),
+        canvasColor: const Color(0xFFfafafa),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(), // const は付けない
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  MyHomePage({Key? key}) : super(key: key); // Key? key と super(key: key) を使用
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  static int _idCounter = 0; // _id ではなく _idCounter にリネームしてカウンターであることを明確に
+  static Map<int, String> _todoList = {}; // TodoのIDと内容を保存
+  static final _controller = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void dispose() {
+    _controller.dispose(); // TextEditingControllerは必ずdisposeする
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Todo App'), // const を追加
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: Column( // Center を削除し、Column を直接使う
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(10.0), // const を追加
+            child: TextField(
+              controller: _controller,
+              style: const TextStyle( // const を追加
+                fontSize: 28.0,
+                color: Color(0xff000000),
+                fontWeight: FontWeight.w400,
+                fontFamily: "Roboto",
+              ),
+              decoration: const InputDecoration( // ヒントテキストを追加
+                hintText: '新しいTodoを入力',
+              ),
             ),
-          ],
-        ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0), // 横方向のパディング
+            child: ElevatedButton(
+              onPressed: _addTodo, // メソッド名を変更
+              child: const Text( // const を追加
+                "追加",
+                style: TextStyle(
+                  fontSize: 32.0,
+                  color: Color(0xff000000),
+                  fontWeight: FontWeight.w400,
+                  fontFamily: "Roboto",
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10), // スペーサー
+          Expanded( // ListViewが使用可能なスペースを全て使うようにする
+            child: _todoList.isEmpty
+                ? const Center( // Todoリストが空の場合のメッセージ
+                    child: Text(
+                      'Todoがありません',
+                      style: TextStyle(fontSize: 20, color: Colors.grey),
+                    ),
+                  )
+                : ListView.builder(
+                    itemCount: _todoList.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      // MapのキーをIDとして取得
+                      final int todoId = _todoList.keys.elementAt(index);
+                      // IDに対応するTodoの内容を取得
+                      final String todoText = _todoList[todoId]!; // ! でnullでないことを保証
+
+                      return Card( // 見た目を良くするためにCardで囲む
+                        margin: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+                        child: CheckboxListTile(
+                          title: Text(
+                            todoText,
+                            style: const TextStyle(fontSize: 20.0),
+                          ),
+                          value: false, // 初期値はfalse（チェックなし）
+                          onChanged: (bool? newValue) {
+                            // newValueがtrue（チェックされた）の場合に削除
+                            if (newValue == true) {
+                              _removeTodo(todoId);
+                            }
+                          },
+                        ),
+                      );
+                    },
+                  ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  // Todoを追加するメソッド
+  void _addTodo() {
+    setState(() {
+      final String text = _controller.text.trim(); // 空白をトリム
+      if (text.isNotEmpty) { // 空文字列でない場合のみ追加
+        _idCounter++;
+        _todoList[_idCounter] = text;
+        _controller.clear(); // テキストフィールドをクリア
+      }
+    });
+  }
+
+  // Todoを削除するメソッド
+  // Todoを削除するメソッド
+  void _removeTodo(int id) {
+    setState(() {
+      // ★★★ 削除する前にTodoの内容を取得する ★★★
+      final String? removedTodoText = _todoList[id];
+
+      _todoList.remove(id); // 指定されたIDのTodoをMapから削除
+      // SnackBarの表示
+      if (removedTodoText != null) { // 取得したテキストがnullでなければ表示
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Todoを削除しました: "$removedTodoText"'), // ここで取得したテキストを使用
+            duration: const Duration(seconds: 1),
+          ),
+        );
+      }
+    });
   }
 }
